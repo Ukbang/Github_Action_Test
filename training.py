c75f59c096ffc214f9d43f7b5e778a2212ac2122
Ukbang <psw1918@naver.com>
2024-09-04 17:03:21 +0900

    Update training.py

--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 
-def training(model, x_train, y_train, x_test=None, y_test=None, epochs=10):
+def training(model, x_train, y_train, x_test=None, y_test=None, epochs=1):
     
     model.compile(optimizer='adam',
                 loss='sparse_categorical_crossentropy',
